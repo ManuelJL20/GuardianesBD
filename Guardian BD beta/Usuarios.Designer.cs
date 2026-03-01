2d13d70f4e528a737652236353b39133144977d6
Manuel Salas <jmsalas20may@gmail.com>
2026-03-01 19:42:09 -0400

    fix: se arreglo que el error que no permitia tenre dos usuarios con la misma clave (Error 2)

--- Guardian BD beta/Usuarios.Designer.cs
+++ Guardian BD beta/Usuarios.Designer.cs
@@ -60,9 +60,10 @@ namespace Guardian_BD_beta
             this.label12.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label12.AutoSize = true;
             this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label12.Location = new System.Drawing.Point(94, 94);
+            this.label12.Location = new System.Drawing.Point(70, 76);
+            this.label12.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label12.Name = "label12";
-            this.label12.Size = new System.Drawing.Size(414, 29);
+            this.label12.Size = new System.Drawing.Size(327, 25);
             this.label12.TabIndex = 163;
             this.label12.Text = "Formulario para nuevos usuarios";
             // 
@@ -70,9 +71,10 @@ namespace Guardian_BD_beta
             // 
             this.button5.BackColor = System.Drawing.Color.Gold;
             this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button5.Location = new System.Drawing.Point(12, 12);
+            this.button5.Location = new System.Drawing.Point(9, 10);
+            this.button5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(161, 45);
+            this.button5.Size = new System.Drawing.Size(121, 37);
             this.button5.TabIndex = 161;
             this.button5.Text = "VOLVER";
             this.button5.UseVisualStyleBackColor = false;
@@ -84,9 +86,10 @@ namespace Guardian_BD_beta
             this.label11.AutoSize = true;
             this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 17F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label11.ForeColor = System.Drawing.Color.Black;
-            this.label11.Location = new System.Drawing.Point(589, 9);
+            this.label11.Location = new System.Drawing.Point(442, 7);
+            this.label11.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label11.Name = "label11";
-            this.label11.Size = new System.Drawing.Size(174, 33);
+            this.label11.Size = new System.Drawing.Size(144, 29);
             this.label11.TabIndex = 160;
             this.label11.Text = "USUARIOS";
             // 
@@ -94,10 +97,11 @@ namespace Guardian_BD_beta
             // 
             this.con.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.con.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.con.Location = new System.Drawing.Point(99, 380);
+            this.con.Location = new System.Drawing.Point(74, 309);
+            this.con.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.con.Name = "con";
             this.con.PasswordChar = '*';
-            this.con.Size = new System.Drawing.Size(240, 36);
+            this.con.Size = new System.Drawing.Size(181, 30);
             this.con.TabIndex = 152;
             // 
             // label10
@@ -105,9 +109,10 @@ namespace Guardian_BD_beta
             this.label10.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label10.AutoSize = true;
             this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label10.Location = new System.Drawing.Point(94, 233);
+            this.label10.Location = new System.Drawing.Point(70, 189);
+            this.label10.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(245, 29);
+            this.label10.Size = new System.Drawing.Size(194, 25);
             this.label10.TabIndex = 151;
             this.label10.Text = "Nombre de usuario";
             // 
@@ -116,9 +121,10 @@ namespace Guardian_BD_beta
             this.label9.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label9.Location = new System.Drawing.Point(94, 348);
+            this.label9.Location = new System.Drawing.Point(70, 283);
+            this.label9.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(153, 29);
+            this.label9.Size = new System.Drawing.Size(124, 25);
             this.label9.TabIndex = 150;
             this.label9.Text = "Contraseña";
             // 
@@ -127,9 +133,10 @@ namespace Guardian_BD_beta
             this.label8.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(94, 461);
+            this.label8.Location = new System.Drawing.Point(70, 375);
+            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(54, 29);
+            this.label8.Size = new System.Drawing.Size(43, 25);
             this.label8.TabIndex = 149;
             this.label8.Text = "Rol";
             // 
@@ -142,9 +149,10 @@ namespace Guardian_BD_beta
             this.rol.Items.AddRange(new object[] {
             "Administrador",
             "Usuario"});
-            this.rol.Location = new System.Drawing.Point(99, 493);
+            this.rol.Location = new System.Drawing.Point(74, 401);
+            this.rol.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.rol.Name = "rol";
-            this.rol.Size = new System.Drawing.Size(240, 37);
+            this.rol.Size = new System.Drawing.Size(181, 33);
             this.rol.TabIndex = 144;
             // 
             // label1
@@ -152,9 +160,10 @@ namespace Guardian_BD_beta
             this.label1.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(-69, 240);
+            this.label1.Location = new System.Drawing.Point(-52, 195);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(39, 29);
+            this.label1.Size = new System.Drawing.Size(34, 25);
             this.label1.TabIndex = 143;
             this.label1.Text = "CI";
             // 
@@ -162,10 +171,11 @@ namespace Guardian_BD_beta
             // 
             this.nom.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.nom.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.nom.Location = new System.Drawing.Point(99, 265);
+            this.nom.Location = new System.Drawing.Point(74, 215);
+            this.nom.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.nom.Name = "nom";
             this.nom.ShortcutsEnabled = false;
-            this.nom.Size = new System.Drawing.Size(240, 36);
+            this.nom.Size = new System.Drawing.Size(181, 30);
             this.nom.TabIndex = 142;
             // 
             // dataGridView1
@@ -173,12 +183,13 @@ namespace Guardian_BD_beta
             this.dataGridView1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(737, 198);
+            this.dataGridView1.Location = new System.Drawing.Point(553, 161);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(621, 392);
+            this.dataGridView1.Size = new System.Drawing.Size(466, 318);
             this.dataGridView1.TabIndex = 141;
             this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
@@ -188,9 +199,10 @@ namespace Guardian_BD_beta
             // 
             this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(1305, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(979, 10);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(151, 133);
+            this.pictureBox1.Size = new System.Drawing.Size(113, 108);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 165;
             this.pictureBox1.TabStop = false;
@@ -200,9 +212,10 @@ namespace Guardian_BD_beta
             this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.button1.BackColor = System.Drawing.Color.Gold;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(99, 606);
+            this.button1.Location = new System.Drawing.Point(74, 492);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(173, 45);
+            this.button1.Size = new System.Drawing.Size(130, 37);
             this.button1.TabIndex = 166;
             this.button1.Text = "AÑADIR";
             this.button1.UseVisualStyleBackColor = false;
@@ -214,9 +227,10 @@ namespace Guardian_BD_beta
             this.button2.BackColor = System.Drawing.Color.Gold;
             this.button2.Enabled = false;
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(278, 606);
+            this.button2.Location = new System.Drawing.Point(208, 492);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(173, 45);
+            this.button2.Size = new System.Drawing.Size(130, 37);
             this.button2.TabIndex = 167;
             this.button2.Text = "BORRAR";
             this.button2.UseVisualStyleBackColor = false;
@@ -228,9 +242,10 @@ namespace Guardian_BD_beta
             this.button3.BackColor = System.Drawing.Color.Gold;
             this.button3.Enabled = false;
             this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(151, 657);
+            this.button3.Location = new System.Drawing.Point(113, 534);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(239, 45);
+            this.button3.Size = new System.Drawing.Size(179, 37);
             this.button3.TabIndex = 168;
             this.button3.Text = "MODIFICAR";
             this.button3.UseVisualStyleBackColor = false;
@@ -245,9 +260,10 @@ namespace Guardian_BD_beta
             this.button4.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.button4.BackColor = System.Drawing.Color.Gold;
             this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.Location = new System.Drawing.Point(99, 138);
+            this.button4.Location = new System.Drawing.Point(74, 112);
+            this.button4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(350, 51);
+            this.button4.Size = new System.Drawing.Size(262, 41);
             this.button4.TabIndex = 169;
             this.button4.Text = "NUEVO USUARIO";
             this.button4.UseVisualStyleBackColor = false;
@@ -257,10 +273,11 @@ namespace Guardian_BD_beta
             // 
             this.conres.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.conres.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.conres.Location = new System.Drawing.Point(737, 153);
+            this.conres.Location = new System.Drawing.Point(553, 124);
+            this.conres.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.conres.Name = "conres";
             this.conres.PasswordChar = '*';
-            this.conres.Size = new System.Drawing.Size(240, 36);
+            this.conres.Size = new System.Drawing.Size(181, 30);
             this.conres.TabIndex = 170;
             this.conres.Visible = false;
             // 
@@ -268,18 +285,19 @@ namespace Guardian_BD_beta
             // 
             this.nomres.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.nomres.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.nomres.Location = new System.Drawing.Point(737, 111);
+            this.nomres.Location = new System.Drawing.Point(553, 90);
+            this.nomres.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.nomres.Name = "nomres";
             this.nomres.PasswordChar = '*';
-            this.nomres.Size = new System.Drawing.Size(240, 36);
+            this.nomres.Size = new System.Drawing.Size(181, 30);
             this.nomres.TabIndex = 171;
             this.nomres.Visible = false;
             // 
             // Usuarios
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1468, 808);
+            this.ClientSize = new System.Drawing.Size(1040, 640);
             this.Controls.Add(this.nomres);
             this.Controls.Add(this.conres);
             this.Controls.Add(this.button4);
@@ -299,6 +317,7 @@ namespace Guardian_BD_beta
             this.Controls.Add(this.nom);
             this.Controls.Add(this.dataGridView1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Usuarios";
             this.Text = "Usuarios";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
